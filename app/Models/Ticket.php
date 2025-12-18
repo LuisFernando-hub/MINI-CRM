51d0290cb726ee94c5ba0d8c4206ebf91ffe365a
Luis Fernando <84160974+LuisFernando-hub@users.noreply.github.com>
2025-12-18 10:52:34 -0300

    add factory for db seeder

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
-        'title',
+        'subject',
         'description',
         'status',
         'customer_id',
